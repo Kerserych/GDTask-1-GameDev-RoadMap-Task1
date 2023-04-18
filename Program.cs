1ca995c807a85ef7fb6bdf8fb38190768839d4a9
Nikita Tsygulev <hekitos007@mail.ru>
2023-04-18 16:04:22 +0300

    Сделал задание

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿object[] bob = new object[] { "15", 3, 2, 5, "22", 4, "33", 15, "15" };
+Console.WriteLine(get(bob));
+
+int get(object[] mas1)
+{
+    List<int> newmas1 = new List<int>();
+
+    int intsum = 0;
+
+    for (int i = 0; i < mas1.Length; i++)
+    {
+        if ((mas1[i] is int == true) & (mas1[i] is string == false))
+        {
+            newmas1.Add((int)mas1[i]);
+        }
+    }
+    //Console.WriteLine(String.Join(" ",newmas1));
+
+    for (int i = 0; i < newmas1.Count; i++)
+    {
+        intsum += newmas1[i];
+    }
+    //Console.WriteLine(intsum);
+    //Console.WriteLine(mas1.Length - newmas1.Count);
+
+    return intsum - newmas1.Count;
+}
